99758a636c3b06d5beec87b3a1b30a309efd0b48
Caleb James DeLisle <cjd@cjdns.fr>
2017-06-29 17:56:15 +0200

    get rid of usage of 'this' because it is called with apply

--- src/matrix.js
+++ src/matrix.js
@@ -19,6 +19,10 @@ if (localStorage == null) {
   localStorage = new LocalStorage('./hubot-matrix.localStorage');
 }
 
+module.exports.use = (robot) => {
+
+let that;
+
 class Matrix extends Adapter {
   constructor() {
     super(...arguments);
@@ -26,6 +30,7 @@ class Matrix extends Adapter {
   }
 
   handleUnknownDevices(err) {
+    let that = this;
     return (() => {
       let result = [];
       for (var stranger in err.devices) {
@@ -34,8 +39,8 @@ class Matrix extends Adapter {
           let result1 = [];
           for (let device in devices) {
             let _ = devices[device];
-            this.robot.logger.info(`Acknowledging ${stranger}'s device ${device}`);
-            result1.push(this.client.setDeviceKnown(stranger, device));
+            that.robot.logger.info(`Acknowledging ${stranger}'s device ${device}`);
+            result1.push(that.client.setDeviceKnown(stranger, device));
           }
           return result1;
         })());
@@ -45,11 +50,10 @@ class Matrix extends Adapter {
   }
 
   send(envelope, ...strings) {
-    var that = this;
     return (() => {
       let result = [];
       for (var str of Array.from(strings)) {
-        this.robot.logger.info(`Sending to ${envelope.room}: ${str}`);
+        that.robot.logger.info(`Sending to ${envelope.room}: ${str}`);
         if (/^(f|ht)tps?:\/\//i.test(str)) {
           result.push(that.sendURL(envelope, str));
         } else {
@@ -67,119 +71,118 @@ class Matrix extends Adapter {
 
   emote(envelope, ...strings) {
     return Array.from(strings).map((str) =>
-      this.client.sendEmoteMessage(envelope.room, str).catch(err => {
+      that.client.sendEmoteMessage(envelope.room, str).catch(err => {
         if (err.name === 'UnknownDeviceError') {
-          this.handleUnknownDevices(err);
-          return this.client.sendEmoteMessage(envelope.room, str);
+          that.handleUnknownDevices(err);
+          return that.client.sendEmoteMessage(envelope.room, str);
         }
       }));
   }
 
   reply(envelope, ...strings) {
     return Array.from(strings).map((str) =>
-      this.send(envelope, `${envelope.user.name}: ${str}`));
+      that.send(envelope, `${envelope.user.name}: ${str}`));
   }
 
   topic(envelope, ...strings) {
     return Array.from(strings).map((str) =>
-      this.client.sendStateEvent(envelope.room, "m.room.topic", {
+      that.client.sendStateEvent(envelope.room, "m.room.topic", {
         topic: str
       }, ""));
   }
 
   sendURL(envelope, url) {
-    this.robot.logger.info(`Downloading ${url}`);
+    that.robot.logger.info(`Downloading ${url}`);
     return request({url, encoding: null}, (error, response, body) => {
       if (error) {
-        return this.robot.logger.info(`Request error: ${JSON.stringify(error)}`);
+        return that.robot.logger.info(`Request error: ${JSON.stringify(error)}`);
       } else if (response.statusCode === 200) {
         let info;
         try {
           let dims = sizeOf(body);
-          this.robot.logger.info(`Image has dimensions ${JSON.stringify(dims)}, size ${body.length}`);
+          that.robot.logger.info(`Image has dimensions ${JSON.stringify(dims)}, size ${body.length}`);
           if (dims.type === 'jpg') { dims.type = 'jpeg'; }
           info = { mimetype: `image/${dims.type}`, h: dims.height, w: dims.width, size: body.length };
-          return this.client.uploadContent(body, {name: url, type: info.mimetype, rawResponse: false, onlyContentUri: true}).done(content_uri => {
-            return this.client.sendImageMessage(envelope.room, content_uri, info, url).catch(err => {
+          return that.client.uploadContent(body, {name: url, type: info.mimetype, rawResponse: false, onlyContentUri: true}).done(content_uri => {
+            return that.client.sendImageMessage(envelope.room, content_uri, info, url).catch(err => {
               if (err.name === 'UnknownDeviceError') {
-                this.handleUnknownDevices(err);
-                return this.client.sendImageMessage(envelope.room, content_uri, info, url);
+                that.handleUnknownDevices(err);
+                return that.client.sendImageMessage(envelope.room, content_uri, info, url);
               }
             });
           });
         } catch (error1) {
           error = error1;
-          this.robot.logger.info(error.message);
-          return this.send(envelope, ` ${url}`);
+          that.robot.logger.info(error.message);
+          return that.send(envelope, ` ${url}`);
         }
       }
     });
   }
 
   run() {
-    this.robot.logger.info(`Run ${this.robot.name}`);
+    that.robot.logger.info(`Run ${that.robot.name}`);
     let client = sdk.createClient(process.env.HUBOT_MATRIX_HOST_SERVER || 'https://matrix.org');
-    this.robot.matrixClient = client;
+    that.robot.matrixClient = client;
     return client.login('m.login.password', {
-      user: process.env.HUBOT_MATRIX_USER || this.robot.name,
+      user: process.env.HUBOT_MATRIX_USER || that.robot.name,
       password: process.env.HUBOT_MATRIX_PASSWORD
     }, (err, data) => {
         if (err) {
-            this.robot.logger.error(err);
+            that.robot.logger.error(err);
             return;
         }
-        this.user_id = data.user_id;
-        this.access_token = data.access_token;
-        this.device_id = data.device_id;
-        this.robot.logger.info(`Logged in ${this.user_id} on device ${this.device_id}`);
-        this.client = sdk.createClient({
+        that.user_id = data.user_id;
+        that.access_token = data.access_token;
+        that.device_id = data.device_id;
+        that.robot.logger.info(`Logged in ${that.user_id} on device ${that.device_id}`);
+        that.client = sdk.createClient({
             baseUrl: process.env.HUBOT_MATRIX_HOST_SERVER || 'https://matrix.org',
-            accessToken: this.access_token,
-            userId: this.user_id,
-            deviceId: this.device_id,
+            accessToken: that.access_token,
+            userId: that.user_id,
+            deviceId: that.device_id,
             sessionStore: new sdk.WebStorageSessionStore(localStorage)
         });
-        this.client.on('sync', (state, prevState, data) => {
+        that.client.on('sync', (state, prevState, data) => {
             switch (state) {
               case "PREPARED":
-                this.robot.logger.info(`Synced ${this.client.getRooms().length} rooms`);
+                that.robot.logger.info(`Synced ${that.client.getRooms().length} rooms`);
                 // We really don't want to let people set the display name to something other than the bot
                 // name because the bot only reacts to it's own name.
-                const currentDisplayName = this.client.getUser(this.user_id).displayName;
-                if (this.robot.name !== currentDisplayName) {
-                  this.robot.logger.info(`Setting display name to ${this.robot.name}`);
-                  this.client.setDisplayName(this.robot.name, ()=>{});
+                const currentDisplayName = that.client.getUser(that.user_id).displayName;
+                if (that.robot.name !== currentDisplayName) {
+                  that.robot.logger.info(`Setting display name to ${that.robot.name}`);
+                  that.client.setDisplayName(that.robot.name, ()=>{});
                 }
-                return this.emit('connected');
+                return that.emit('connected');
             }
         });
-        this.client.on('Room.timeline', (event, room, toStartOfTimeline) => {
+        that.client.on('Room.timeline', (event, room, toStartOfTimeline) => {
             if ((event.getType() === 'm.room.message') && (toStartOfTimeline === false)) {
-                this.client.setPresence("online");
+                that.client.setPresence("online");
                 let message = event.getContent();
                 let name = event.getSender();
                 let prettyname = room.currentState._userIdsToDisplayNames[name];
-                let user = this.robot.brain.userForId(name, { name: prettyname });
+                let user = that.robot.brain.userForId(name, { name: prettyname });
                 user.room = room.roomId;
-                if (user.id !== this.user_id) {
-                    this.robot.logger.info(`Received message: ${JSON.stringify(message)} in room: ${user.room}, from: ${user.name}.`);
-                    if (message.msgtype === "m.text") { this.receive(new TextMessage(user, message.body)); }
-                    if ((message.msgtype !== "m.text") || (message.body.indexOf(this.robot.name) !== -1)) { return this.client.sendReadReceipt(event); }
+                if (user.id !== that.user_id) {
+                    that.robot.logger.info(`Received message: ${JSON.stringify(message)} in room: ${user.room}, from: ${user.name}.`);
+                    if (message.msgtype === "m.text") { that.receive(new TextMessage(user, message.body)); }
+                    if ((message.msgtype !== "m.text") || (message.body.indexOf(that.robot.name) !== -1)) { return that.client.sendReadReceipt(event); }
                   }
               }
         });
-        this.client.on('RoomMember.membership', (event, member) => {
-            if ((member.membership === 'invite') && (member.userId === this.user_id)) {
-                return this.client.joinRoom(member.roomId).done(() => {
-                    return this.robot.logger.info(`Auto-joined ${member.roomId}`);
+        that.client.on('RoomMember.membership', (event, member) => {
+            if ((member.membership === 'invite') && (member.userId === that.user_id)) {
+                return that.client.joinRoom(member.roomId).done(() => {
+                    return that.robot.logger.info(`Auto-joined ${member.roomId}`);
                 });
               }
         });
-        return this.client.startClient(0);
+        return that.client.startClient(0);
     });
   }
 }
-
-module.exports.use = (robot) => {
-    return new Matrix(robot);
+    that = new Matrix(robot);
+    return that;
 };
